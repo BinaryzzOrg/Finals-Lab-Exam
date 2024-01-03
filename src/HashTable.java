7558b1e7ebe6474f2609db96b0cc3a69a715f28d
happypoot300 <happypoot300@gmail.com>
2024-01-03 22:09:43 +0800

    first commit bish

---
+++ src/HashTable.java
@@ -0,0 +1,23 @@
+public class HashTable {
+	// field variables
+	int[] Table;
+
+	public HashTable() {
+		int defaultSize = 20;
+
+	}// end constructor
+
+	public void Add() {
+		System.out.print("Enter a number: ");
+
+		int key = Main.CheckUserInput("Enter a number: ");
+
+		// code here
+	}// end method
+
+	public void ViewHashTable() {
+		System.out.println("testcode plz end my suffering :(   ");
+		// code here
+	}// end method
+
+}// end main
