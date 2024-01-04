7827318f8d1272735c475f0138f163e9be2cc960
KuyaWellRevealYummy <reyeskierchristian64@gmail.com>
2024-01-04 11:39:13 +0800

    hashtable
    hashbrown

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,23 +1,68 @@
+
 public class HashTable {
 	// field variables
-	int[] Table;
+	private int[] hashtable;
+
+	public int[] getHashtable() {
+		return hashtable;
+	}
 
 	public HashTable() {
-		int defaultSize = 20;
+		this.hashtable = new int[20];
+	}
 
-	}// end constructor
+	private int performHashKeyFunction(int value) {
+		return value % 20;
+	}
+	
+	private void performLinearProbing(int hashkey, int value) {
+		while (getHashtable()[hashkey] != 0 || getHashtable()[hashkey] == value) {
 
-	public void Add() {
-		System.out.print("Enter a number: ");
+			hashkey = (hashkey + 1) % 20;
+		}
 
-		int key = Main.CheckUserInput("Enter a number: ");
+		getHashtable()[hashkey] = value;
 
-		// code here
-	}// end method
+	}
 
-	public void ViewHashTable() {
-		System.out.println("testcode plz end my suffering :(   ");
-		// code here
-	}// end method
+	public void put(int value) {
 
+		int hashkey = performHashKeyFunction(value);
+		performLinearProbing(hashkey, value);
+
+	}
+
+	public String setGrid(char symbol, int numTimes) {
+		String grid = "";
+		for (int idxForDesigning = 0; idxForDesigning < numTimes; idxForDesigning++) {
+			grid += symbol;
+		}
+
+		grid += "\n";
+
+		return grid;
+
+	}
+
+	public String toString() {
+		String concat = "Hash Table\n";
+		concat += setGrid('_', 20);
+
+		for (int hashkey = 0; hashkey < 20; hashkey++) {
+			concat += hashkey + "     |       ";
+
+			if (getHashtable()[hashkey] != 0) {
+				concat += hashtable[hashkey] + "\n" + setGrid('_', 20);
+				System.out.println();
+			} else {
+				concat += "\t\n" + setGrid('_', 20);
+			}
+
+			System.out.println();
+
+		}
+
+		return concat;
+
+	}
 }// end main
