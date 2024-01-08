1758035e6f907ea83570beb41334ca2410fa8f82
KuyaWellRevealYummy <reyeskierchristian64@gmail.com>
2024-01-08 20:06:46 +0800

    unique

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,9 +1,9 @@
 
 public class HashTable {
 	/*
-	 * int[] hashtable = the array table with a size of 17
-	 * int DEFAULTSIZE = the size of the table also the n for the formula of hashsum which is k % n
-	 * int nTotalItemsinTable = current total items inside the table
+	 * int[] hashtable = the array table with a size of 17 int DEFAULTSIZE = the
+	 * size of the table also the n for the formula of hashsum which is k % n int
+	 * nTotalItemsinTable = current total items inside the table
 	 */
 	private int[] hashtable;
 	private static final int DEFAULTSIZE = 17;
@@ -17,44 +17,46 @@ public class HashTable {
 		this.hashtable = new int[DEFAULTSIZE];
 		fillFirstWithNeg(hashtable, -1);
 	}
-	
+
 	/*
-	 * Purpose is for printing the values and determining the collisions
-	 * If not (!=) -1 then means it has already taken so thus a collision
-	 * It set first the empty getHashtable with -1    
+	 * Purpose is for printing the values and determining the collisions If not (!=)
+	 * -1 then means it has already taken so thus a collision It set first the empty
+	 * getHashtable with -1
 	 */
-	
+
 	public void fillFirstWithNeg(int[] ArrayToBeFill, int fillWith) {
 		for (int idxOfElem = 0; idxOfElem < ArrayToBeFill.length; idxOfElem++) {
 			ArrayToBeFill[idxOfElem] = fillWith;
 		}
-		
+
 	}
-	
+
 	/*
-	 * Since the formula given in the instructions is n % 17
-	 * this method returns the value modulo DEFAULTSIZE or 17
-	 * returns it in int value
+	 * Since the formula given in the instructions is n % 17 this method returns the
+	 * value modulo DEFAULTSIZE or 17 returns it in int value
 	 */
-	
+
 	private int ComputeHashSum(int value) {
 		return value % DEFAULTSIZE;
 	}
-	
+
 	/*
-	 * Performs linear probing
-	 * we use ++hashkey or the pre-incremented hashkey since for printing the next candidate slot
-	 * also the getHashtable()[hashkey] = value is used so that if it reach the end it will go back to index 0 to repeat the searching for empty slot
-	 * for instance we are in the last spot and it's pre incremented and we know that an array is 0-16 (when n-size 17) 
-	 * so we preincremented it to go to 17 to perform the 17 % 17 = 0 thus the cycle continues
-	 * After that if that spot is empty which is determined by the doing the fillFirstWithNeg method (turns the empty array into -1 upon invoking constructor)
-	 * it's gonna put the value to that empty spot and increment its nItems to update the item status
+	 * Performs linear probing we use ++hashkey or the pre-incremented hashkey since
+	 * for printing the next candidate slot also the getHashtable()[hashkey] = value
+	 * is used so that if it reach the end it will go back to index 0 to repeat the
+	 * searching for empty slot for instance we are in the last spot and it's pre
+	 * incremented and we know that an array is 0-16 (when n-size 17) so we
+	 * preincremented it to go to 17 to perform the 17 % 17 = 0 thus the cycle
+	 * continues After that if that spot is empty which is determined by the doing
+	 * the fillFirstWithNeg method (turns the empty array into -1 upon invoking
+	 * constructor) it's gonna put the value to that empty spot and increment its
+	 * nItems to update the item status
 	 */
-	
+
 	private void performLinearProbing(int hashkey, int value) {
 		while (getHashtable()[hashkey] != -1) {
 			++hashkey;
-			System.out.println("Collision Spotted, performing Linear Probing...\nLet's try with " + hashkey);
+			System.out.println("Collision Spotted, performing Linear Probing...\nLet's try with table index " + hashkey);
 			hashkey %= DEFAULTSIZE;
 		}
 
@@ -62,11 +64,12 @@ public class HashTable {
 		nTotalItemsinTable++;
 
 	}
-	
+
 	/*
 	 * basically the methods inside here are explain on previous comments
-	 * ComputeHashSum returns the hashsum stores it in hashkey
-	 * performLinearProbing checks if there's collision and also process putting the values to the table array
+	 * ComputeHashSum returns the hashsum stores it in hashkey performLinearProbing
+	 * checks if there's collision and also process putting the values to the table
+	 * array
 	 */
 
 	public void put(int value) {
@@ -80,75 +83,90 @@ public class HashTable {
 		for (int idxForDesigning = 0; idxForDesigning < numTimes; idxForDesigning++) {
 			System.out.print(symbol);
 		}
-		
+
 		System.out.println();
 	}
 	
 	/*
-	 * We use the initResize so that the values will only iterates for 10 times after that it will create now a another visual look of table
-	 * we do this since its gonna be long in the console if it printed one value per 17 times (as the size) vertically or horizontally
-	 * setGrid is just gonna print a char symbol for further designing
-	 * in the first line on the method we use 2 since 17 % 10 also 17 is greater so it needs another table  
-	 * Basically it means that it will create a two tables from 0-9 to 10-16
-	 * we check if the value and index is < 10 to format the single digit or double digit 
-	 * it checks also if its less than to defaultsize since printing by 17 will cause out of bounds
-	 * it means this line of code will check if the value is only less than to default size so that we only print the values inside of that size 
-	 * if not it just gonna print whitespaces or | to beautify formatting
-	 * Lastly it will print the totalitems and size
+	 * The given instruction requires a 10 unique elements 
+	 * contains method iterates through the table to check if a value is equal to the number
+	 * returns true if not unique while false if considered unique
+	 */
+	public boolean contains(int inputNumber) {
+		for (int valueInTable: getHashtable()) {
+			if (valueInTable == inputNumber) return true;
+		}
+		
+		return false;
+	}
+
+	/*
+	 * We use the initResize so that the values will only iterates for 10 times
+	 * after that it will create now a another visual look of table we do this since
+	 * its gonna be long in the console if it printed one value per 17 times (as the
+	 * size) vertically or horizontally setGrid is just gonna print a char symbol
+	 * for further designing in the first line on the method we use 2 since 17 % 10
+	 * also 17 is greater so it needs another table Basically it means that it will
+	 * create a two tables from 0-9 to 10-16 we check if the value and index is < 10
+	 * to format the single digit or double digit it checks also if its less than to
+	 * defaultsize since printing by 17 will cause out of bounds it means this line
+	 * of code will check if the value is only less than to default size so that we
+	 * only print the values inside of that size if not it just gonna print
+	 * whitespaces or | to beautify formatting Lastly it will print the totalitems
+	 * and size
 	 */
 
 	public void ViewingTheTable(int initResize) {
 		setGrid('-', 51);
 
-		System.out.println("Hash Table with size " + DEFAULTSIZE);
-		
+		System.out.println("\nHash Table with size " + DEFAULTSIZE);
+
 		for (int i = 0; i < 2; i++) {
-			
+
 			initResize += 10;
-			
+
 			setGrid('-', 51);
-			
-			for (int idxForPrintingtheVal = initResize - 10; idxForPrintingtheVal < initResize; idxForPrintingtheVal++) {
-				 if (idxForPrintingtheVal < DEFAULTSIZE) {
-				if (getHashtable()[idxForPrintingtheVal] == -1) {
-					System.out.print("|    ");
-				} else {
-					 if (getHashtable()[idxForPrintingtheVal] < 10) {
-					        System.out.print("| " + getHashtable()[idxForPrintingtheVal] + "  ");
-					    } else {
-					        System.out.print("| " + getHashtable()[idxForPrintingtheVal] + " ");
-					        
-					    }
-					    }
+
+			for (int idxForPrintingtheVal = initResize
+					- 10; idxForPrintingtheVal < initResize; idxForPrintingtheVal++) {
+				if (idxForPrintingtheVal < DEFAULTSIZE) {
+					if (getHashtable()[idxForPrintingtheVal] == -1) {
+						System.out.print("|    ");
+					} else {
+						if (getHashtable()[idxForPrintingtheVal] < 10) {
+							System.out.print("| " + getHashtable()[idxForPrintingtheVal] + "  ");
+						} else {
+							System.out.print("| " + getHashtable()[idxForPrintingtheVal] + " ");
+
+						}
+					}
 				} else {
 					System.out.print("|    ");
 				}
 			}
-			
-			System.out.println("|");
-			setGrid('-', 51);
-			
-			
-			for (int idxForTheindexPrint = initResize - 10; idxForTheindexPrint < initResize ; idxForTheindexPrint++ ) {
-				 if (idxForTheindexPrint < DEFAULTSIZE) {
-				 if (idxForTheindexPrint < 10) {
-					 
-				        System.out.print("| " + idxForTheindexPrint + "  ");
-				    } else {
-				        System.out.print("| " + idxForTheindexPrint + " ");
-				    }
-				 
-				 } else {
-					 System.out.print("|    ");
-				 }
-			}
-		
 
 			System.out.println("|");
 			setGrid('-', 51);
-			
+
+			for (int idxForTheindexPrint = initResize - 10; idxForTheindexPrint < initResize; idxForTheindexPrint++) {
+				if (idxForTheindexPrint < DEFAULTSIZE) {
+					if (idxForTheindexPrint < 10) {
+
+						System.out.print("| " + idxForTheindexPrint + "  ");
+					} else {
+						System.out.print("| " + idxForTheindexPrint + " ");
+					}
+
+				} else {
+					System.out.print("|    ");
+				}
+			}
+
+			System.out.println("|");
+			setGrid('-', 51);
+
 		}
-		
+
 		System.out.println("Total items: " + nTotalItemsinTable);
 		System.out.println("Slots as of now: " + (DEFAULTSIZE - nTotalItemsinTable));
 
--- src/Main.java
+++ src/Main.java
@@ -1,86 +1,134 @@
 import java.util.Scanner;
 
 public class Main {
-
-	public static void runnable(HashTable hashtable) {
-
-		hashtable.setGrid('-', 60);
-		System.out.println("\tWelcome to Java Hash-Table");
-		hashtable.setGrid('-', 60);
-		System.out.println("\tOperations");
-		hashtable.setGrid('-', 60);
-
-		System.out.println("\t[1] Put (Put a value in the table)");
-		System.out.println("\t[2] View (View the table)");
-		System.out.println("\t[3] Terminate (Close the program)");
-
-		hashtable.setGrid('-', 60);
-
-		System.out.println("Response: ");
-
-	}
-	/*
-	 * Simple menu driven program
-	 * has a do while to repeat iteration of process
-	 * has a checker to and prompt 10 times only as the instruction provided
-	 * validation of range from 1-30
-	 */
+	// field variables
+	private static HashTable hashTable = new HashTable();
 
 	public static void main(String[] args) {
-		HashTable hashtable = new HashTable();
-		Scanner sc = new Scanner(System.in);
+		Menu();
+	}// end method
 
-		do {
-			runnable(hashtable);
+	/*
+	 * The PrintMenuChoices method returns a formatted string for MainMenu. This
+	 * formatted string is displayed to the user when they run the code for the
+	 * first time. The method is also passed as a parameter to other methods that
+	 * use the 'prompt' String.
+	 */
+	public static String PrintMenuChoices() {
+		String MenuChoicesAsString =
+		//@formatter:off
+								"\n" + 
+								"       {MainMenu}\n"+ 
+								"━━━━━━━━━━━━━━━━━━━━━━━━━\n"+ 
+								"┃ 【 1 】 Add	 \n" +
+								"┃ 【 2 】 ViewHashTable \n" + 
+								"┃ 【 3 】 Exit \n" + 
+								"━━━━━━━━━━━━━━━━━━━━━━━━━\n" + 
+								"》 ";
+				//@formatter:on
+		return MenuChoicesAsString;
+	}// end method
 
-			while (!sc.hasNextInt()) {
-				System.out.println("Wrong data type input!!!\nCorrect inputs: 1, 34, 5");
-				sc.next();
-			}
+	/*
+	 * The MenuChoices method contains the following operations: Add, ViewHashTable,
+	 * and Exit. This method calls the PrintMenuChoices that prints out the choices
+	 * for modifying the HashTable that is chosen by the user. MenuChoices method
+	 * also handles miss inputs of the user and loops if it detects one.
+	 */
+	public static void Menu() {
+		System.out.print(PrintMenuChoices());
+		
+		/*
+		 * case 1 checks first if the first inputted number corresponds to its data type
+		 * next it will check if the hashtable if it constains the same value 
+		 */
+		switch (CheckUserInput(PrintMenuChoices())) {
+		case 1: {// AddA
+			int Counter = 10;
+			int valueToCheckBeforePutting;
+			for (int index = 0; index < Counter; index++) {
+				while (true) {
+					System.out.print("Enter a number : ");
+					valueToCheckBeforePutting = CheckUserInput("Enter a number : ");
 
-			int userChoice = sc.nextInt();
-
-			switch (userChoice) {
-			case 1:
-				hashtable.setGrid('-', 60);
-				
-				int userInputtedNum;
-				System.out.println("Enter numbers ranging 1 - 30 only\nAccepting 10 numbers only:  ");
-
-				for (int idxForPrompting = 0; idxForPrompting < 10; idxForPrompting++) {
-					
-					while (true) {
-						userInputtedNum = sc.nextInt();
-						if (userInputtedNum >= 1 && userInputtedNum <= 30) {
-							break;
-						} else {
-							System.out.println("Accepting values from 1- 30 only ");
-						}
-						
+					if (hashTable.contains(valueToCheckBeforePutting)) {
+						System.out.println("Number is not unique, already in the table");
+					} else {
+						break;
 					}
-					System.out.println("That's " + (idxForPrompting + 1) + ", Keep typing number!!!");
-					hashtable.put(userInputtedNum);
+
 				}
 
-				break;
+				hashTable.put(valueToCheckBeforePutting);
 
-			case 2:
-				
-				hashtable.ViewingTheTable(0);
+			} // end for
 
-				break;
+			hashTable.ViewingTheTable(0);
+			break;
+		}
 
-			case 3:
-				System.out.println("Terminate.....");
-				System.exit(0);
+		case 2: {// ViewHashTable
+			hashTable.ViewingTheTable(0);
+			break;
+		}
 
-				break;
+		case 3: {// Exit
+			System.out.println("「Exiting now...」");
+			System.exit(0);
+			break;
+		}
 
-			default:
-				System.out.println("Wrong Input");
-			}
+		default:
+			// @formatter:off
+			System.out.println("\n" +
+					"⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃\n" +
+					"┇ Error: \n" +
+					"┇ Input is not a valid Menu choice. \n" +
+					"⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃\n" +
+					"┇ Msg: \n" +
+					"┇ \033[3mPlease enter only 1 to 3 as input\033[0m \n" +
+					"⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃");
+			// @formatter:on
+			break;
+		}// end method
 
-		} while (true);
-
+		Menu();
+	}// end method
+	
+	
+	public static void CheckUniqueNess() {
+		
+		
 	}
-}
+	
+	
+	
+	/*
+	 * The CheckUserInput method scans the user's input and checks if it is an
+	 * integer. If the input is an integer, it is stored in the 'value' variable and
+	 * returns it. If the input is not an integer, an error message is displayed,
+	 * and the user is prompted to enter an integer value. The 'prompt' parameter is
+	 * used for different scenarios of printing needed for certain menus.
+	 */
+	public static int CheckUserInput(String prompt) {
+		Scanner sc = new Scanner(System.in);
+
+		if (sc.hasNextInt()) {
+			int value = sc.nextInt();
+			return value;
+		} // end if
+
+		// @formatter:off
+			System.out.println("\n" +
+						"⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃\n" +
+						"┇ Error: \n" +
+						"┇ Input is not an integer value. \n" +
+						"⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃\n" +
+						"┇ Msg: \n" +
+						"┇ \033[3mPlease enter INTEGER input only\033[0m \n" +
+						"⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃\n");
+		// @formatter:on
+		System.out.print(prompt);
+		return CheckUserInput(prompt);
+	}// end if
+}// end method
