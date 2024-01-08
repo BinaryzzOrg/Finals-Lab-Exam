f3b14335fe75bf8936b16ccff19224f0784b8292
happypoot300 <happypoot300@gmail.com>
2024-01-08 20:52:05 +0800

    fixed bato

--- src/Main.java
+++ src/Main.java
@@ -8,96 +8,6 @@ public class Main {
 		Menu();
 	}// end method
 
-
-	/*
-	 * The PrintMenuChoices method returns a formatted string for MainMenu. This
-	 * formatted string is displayed to the user when they run the code for the
-	 * first time. The method is also passed as a parameter to other methods that
-	 * use the 'prompt' String.
-	 */
-	public static String PrintMenuChoices() {
-		String MenuChoicesAsString =
-		//@formatter:off
-								"\n" + 
-								"       {MainMenu}\n"+ 
-								"━━━━━━━━━━━━━━━━━━━━━━━━━\n"+ 
-								"┃ 【 1 】 Add	 \n" +
-								"┃ 【 2 】 ViewHashTable \n" + 
-								"┃ 【 3 】 Exit \n" + 
-								"━━━━━━━━━━━━━━━━━━━━━━━━━\n" + 
-								"》 ";
-				//@formatter:on
-		return MenuChoicesAsString;
-	}// end method
-
-	/*
-	 * The MenuChoices method contains the following operations: Add, ViewHashTable,
-	 * and Exit. This method calls the PrintMenuChoices that prints out the choices
-	 * for modifying the HashTable that is chosen by the user. MenuChoices method
-	 * also handles miss inputs of the user and loops if it detects one.
-	 */
-	public static void Menu() {
-		System.out.print(PrintMenuChoices());
-		
-		/*
-		 * case 1 checks first if the first inputted number corresponds to its data type
-		 * next it will check if the hashtable if it constains the same value 
-		 */
-		switch (CheckUserInput(PrintMenuChoices())) {
-		case 1: {// AddA
-			int Counter = 10;
-			int valueToCheckBeforePutting;
-			for (int index = 0; index < Counter; index++) {
-				while (true) {
-					System.out.print("Enter a number : ");
-					valueToCheckBeforePutting = CheckUserInput("Enter a number : ");
-
-					if (hashTable.contains(valueToCheckBeforePutting)) {
-						System.out.println("Number is not unique, already in the table");
-					} else {
-						break;
-					}
-
-				}
-
-				hashTable.put(valueToCheckBeforePutting);
-
-			} // end for
-
-			hashTable.ViewingTheTable(0);
-			break;
-		}
-
-		case 2: {// ViewHashTable
-			hashTable.ViewingTheTable(0);
-			break;
-		}
-
-		case 3: {// Exit
-			System.out.println("「Exiting now...」");
-			System.exit(0);
-			break;
-		}
-
-		default:
-			// @formatter:off
-			System.out.println("\n" +
-					"⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃\n" +
-					"┇ Error: \n" +
-					"┇ Input is not a valid Menu choice. \n" +
-					"⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃\n" +
-					"┇ Msg: \n" +
-					"┇ \033[3mPlease enter only 1 to 3 as input\033[0m \n" +
-					"⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃⁃");
-			// @formatter:on
-			break;
-		}// end method
-
-		Menu();
-	}// end method
-	
-	
-	
 	/*
 	 * The PrintMenuChoices method returns a formatted string for MainMenu. This
 	 * formatted string is displayed to the user when they run the code for the
@@ -196,8 +106,4 @@ public class Main {
 		System.out.print(prompt);
 		return CheckUserInput(prompt);
 	}// end if
-
 }// end method
-
-}// end method
-
